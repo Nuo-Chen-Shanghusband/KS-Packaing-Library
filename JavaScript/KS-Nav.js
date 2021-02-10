e0828fd5703b85271722050941a7e64cc9b8d327
NuoChen <kechenweicn@126.com>
2021-02-10 12:09:55 +0800

    master

--- JavaScript/KS-Nav.js
+++ JavaScript/KS-Nav.js
@@ -1,12 +1,14 @@
-function Tree_navigation() {
-	//树状导航
-	document.getElementsByClassName('KS-Nav_Tree-navigation_2-parent_i').addEventListener('click',I_Xunzhun());
-	document.getElementsByClassName('KS-Nav_Tree-navigation_3-parent_i').addEventListener('click',I_Xunzhun());
-	document.getElementsByClassName('KS-Nav_Tree-navigation_4-parent_i').addEventListener('click',I_Xunzhun());
-	//对i标签进行事件监听
-	function I_Xunzhun() {
-		document.getElementsByClassName('KS-Nav_Tree-navigation_2-parent_i').style.transform = 'rotate(45deg)';
-		document.getElementsByClassName('KS-Nav_Tree-navigation_3-parent_i').style.transform = 'rotate(45deg)';
-		document.getElementsByClassName('KS-Nav_Tree-navigation_4-parent_i').style.transform = 'rotate(45deg)';
+function Tree_naviagation() {
+	document.getElementsByClassName("KS-Nav_Tree-navigation_2-parent_i[0]").addEventListener("click",I2())
+	function I2() {
+		const istyle = document.getElementsByClassName("KS-Nav_Tree-navigation_2-parent_i");
+	    const length;
+	    for (length = 0; length < istyle.length; length++) {
+		    istyle[length].style.transform = 'rotate(45deg)';
+			document.getElementsByClassName("KS-Nav_Tree-navigation_2-parent_section[0]").style.display = "block"
+
 	}
 }
+	}
+
+Tree_naviagation();
